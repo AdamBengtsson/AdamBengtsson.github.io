e1ee311746e199d97498bf6f19ee2d21f17a4723
Duqil <adam.bengtsson1@hotmail.com>
2026-09-21 11:52:29 +0000

    Split firebase config into its own file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyDn60Wn-VFw8bN8qTFLZWoqWyb75ywNUao",
+  authDomain: "cardnamer.firebaseapp.com",
+  projectId: "cardnamer",
+  storageBucket: "cardnamer.firebasestorage.app",
+  messagingSenderId: "714456423379",
+  appId: "1:714456423379:web:45edfb4d52f808e53e2d26",
+  measurementId: "G-REER6KWHRM"
+};
